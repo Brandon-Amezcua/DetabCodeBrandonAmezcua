78b9ec891492f5f70eb338e0b2c187d4569ef104
Gavin <graygavin11@csu.fullerton.edu>
2020-02-05 14:13:04 -0800

    made changes to the endtab file

--- entab.c
+++ entab.c
@@ -8,13 +8,31 @@ int main (int argc, const char* argv[]) {
   FILE* fin;
   FILE* fout;
   int n;
-
+  printf("Test 1\n");
   bool ok = openfiles(argc, argv, &fin, &fout, &n);
   if (!ok) {exit(1);}
-
-  int c, tabs;
+printf("Test 2\n");
+  int c;
   while ((c = fgetc(fin)) != EOF) {
-    if (c != ' ' && c != '\t') {
+
+    if (c == ' ') {
+      int count = 1;
+      while ((c = fgetc(fin)) != EOF && (c = fgetc(fin)) == ' ') {
+        ++count;
+      }
+      int ntabs = count / n;
+      int nspaces = count % 2;
+      while (ntabs-- > 0) {
+        fputc('\t', fout);
+      }
+      while (nspaces-- > 0) {
+        fputc(' ', fout);
+      }
+    } else {
+      fputc(c, fout);
+    }
+
+    /*if (c != ' ' && c != '\t') {
       fputc(c, fout);
     } else if (c == ' ') {
       tabs++;
@@ -29,7 +47,7 @@ int main (int argc, const char* argv[]) {
       fputc(c,fout);
     } else {
       fputc(c, fout);
-    }
+    }*/
   }
 
   closefiles(fin, fout);
